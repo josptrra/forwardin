9e0a114507dd1ede91bf45c71fd4d320e885b588
Julio Syah Putra <juliosp2107@gmail.com>
2024-09-13 22:51:20 +0700

    feat: custom port server in config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: { port: 3000 },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
